eba2503524781b3d96f65681cfffa1b0aff1d9ae
Dominic Michalec <dominicjjmichalec@gmail.com>
2016-08-14 13:21:31 -0400

    add content to static home and help page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   get 'home', to: 'static_pages#home'
-  get 'about', to: 'static_pages#help'
+  get 'help', to: 'static_pages#help'
   root 'application#hello'
 end
