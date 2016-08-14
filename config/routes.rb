7e663202fd62277bdbc69fa9ebecc6c3a14d26a3
Dominic Michalec <dominicjjmichalec@gmail.com>
2016-08-14 13:10:10 -0400

    add home and about routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-
-  get 'static_pages/help'
-
+  get 'home', to: 'static_pages#home'
+  get 'about', to: 'static_pages#help'
   root 'application#hello'
 end
