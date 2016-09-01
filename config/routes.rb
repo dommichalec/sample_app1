8da7e9c4ace575e868a2ed8133fadb168a8e7610
Dominic Michalec <dominicjjmichalec@gmail.com>
2016-09-01 06:46:08 -0400

    complete simple log in and log out

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   get '/about',    to:    'static_pages#about'
   get '/contact',  to:    'static_pages#contact'
   get 'login', to: 'session#new'
-  post 'login', to: 'session#create'
+  post 'login_post', to: 'session#create'
   delete 'logout', to: 'session#destroy'
 end
